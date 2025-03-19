ea004f328ef6e0a632360485a77229bb319c0ab1
Sharon Chepngeno <chepngenokorir19@gmail.com>
2025-03-19 15:46:21 +0300

    initiate commit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 =31;
+let num2 =2;
 
-// Write your code here
+let multiply = num1 * num2
+
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 =29;
+const num4 =5;
+const mod = num3 % num4
+
+const max = Math.max(1, 20)
